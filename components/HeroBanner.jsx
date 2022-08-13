3387f439dcb32e9173d7a651efb6ba1362dcfb09
Realharpi <harpiwarrach@gmail.com>
2022-08-13 09:52:10 +0200

    Finished the HeroBanner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import Link from 'next/link';
-
 import { urlFor } from '../lib/client';
 
+// Receiving Banner from sanity as a prop here.
 const HeroBanner = ({ heroBanner }) => {
   return (
     <div className="hero-banner-container">
+
+
       <div>
+      {/* Small Text */}
         <p className="beats-solo">{heroBanner.smallText}</p>
+
+        {/* Medium Text */}
         <h3>{heroBanner.midText}</h3>
+
+        {/* All Caps Text */}
         <h1>{heroBanner.largeText1}</h1>
+
+        {/* Banner Image */}
         <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
 
+        {/* Button with a Link. */}
         <div>
           <Link href={`/product/${heroBanner.product}`}>
             <button type="button">{heroBanner.buttonText}</button>
           </Link>
+
+          {/* Description at the bottom-right corner */}
           <div className="desc">
             <h5>Description</h5>
             <p>{heroBanner.desc}</p>
           </div>
+          
         </div>
       </div>
     </div>
